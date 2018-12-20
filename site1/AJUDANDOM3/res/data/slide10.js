d7bb416838702594ca28161787ce8f163735b56c
movatrademkt <46026426+movatrademkt@users.noreply.github.com>
2018-12-20 15:04:32 -0200

    Add files via upload

---
+++ site1/AJUDANDOM3/res/data/slide10.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{BC07B75C-C2AC-4D05-A021-0EBCD6C7949C}'];loadHandler&&loadHandler(9, '<div id="spr0_16f867c"><div id="spr1_16f867c" class="kern slide"><img id="img0_16f867c" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_16f867c" class="kern slide"><div id="vd0_16f867c"></div></div></div>');})();
